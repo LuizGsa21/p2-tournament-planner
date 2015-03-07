66a5d2e5e72b1eab329381a807c2b3d701ae1bcc
Luiz Arantes Sa <LArantesSa@icloud.com>
2015-03-07 15:49:35 -0600

    Add database schema

--- tournament/tournament.sql
+++ tournament/tournament.sql
@@ -7,3 +7,24 @@
 -- these lines here.
 
 
+CREATE TABLE players (
+  id   SERIAL PRIMARY KEY,
+  name TEXT
+);
+
+CREATE TABLE matches (
+  id        SERIAL PRIMARY KEY,
+  winner_id INTEGER REFERENCES players (id),
+  loser_id  INTEGER REFERENCES players (id)
+);
+
+CREATE VIEW standings AS
+  SELECT
+    p.id                                                                AS player_id,
+    p.name                                                              AS player_name,
+    count(CASE WHEN p.id = m.winner_id THEN 1 END)                      AS wins,
+    count(CASE WHEN p.id = m.winner_id OR p.id = m.loser_id THEN 1 END) AS totat_matches
+  FROM players AS p
+    LEFT JOIN matches AS m
+      ON p.id = m.winner_id OR p.id = m.loser_id
+  GROUP BY p.id, p.name;
